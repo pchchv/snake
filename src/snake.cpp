8fc68a369b990cd7f1259cd60bd35fdcd3fefd5b
pchchv <ipchchv@gmail.com>
2022-11-11 19:48:13 +0300

    Implement a snake body

---
+++ src/snake.cpp
@@ -0,0 +1,13 @@
+#include "snake.hpp"
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+SnakeBody::SnakeBody()
+{
+}
+
+SnakeBody::SnakeBody(int x, int y) : mX(x), mY(y)
+{
+}
